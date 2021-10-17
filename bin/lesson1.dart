42d146d5899ed6fb85dafdaba1193dab5be27a2d
ermekadiev94 <ermash.adiev@gmail.com>
2021-10-17 22:34:59 +0600

    till task 6

--- bin/lesson1.dart
+++ bin/lesson1.dart
@@ -1,11 +1,63 @@
 void main() {
-  var a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //Задание 1
+  // var finger = 4;
+  // if (finger == 1) {
+  //   print('Большой палец');
+  // } else if (finger == 2) {
+  //   print('Указательный палец');
+  // } else if (finger == 3) {
+  //   print('Средний палец');
+  // } else if (finger == 4) {
+  //   print('Безымянный палец');
+  // } else if (finger == 5) {
+  //   print('Мезинец');
+  // } else {
+  //   print('error');
+  // }
 
-  var result = [];
+  // Задание 2
+  // var a = [2, 3, 4, 5];
+  // var result = 1;
 
-  for (int i = 0; i < a.length; i++) {
-    if (a[i].isEven) {
-      result.add(a[i]);
-    }
-  }
+  // for (int i = 0; i < a.length; i++) {
+  //   result = result * a[i];
+  // }
+  // print(result);
+
+  // Задание 3
+  // var min = 30;
+  // if (min <= 15) {
+  //   print('Четверть 1');
+  // } else if (min <= 30 && min > 15) {
+  //   print('Четверть 2');
+  // } else if (min <= 45 && min > 30) {
+  //   print('Четверть 3');
+  // } else if (min <= 59 && min > 45) {
+  //   print('Четверть 4');
+  // }
+
+  //  Задание 4
+  // var arr = [12, 15, 20, 25, 59, 79];
+
+  // var result1 = 0;
+  // double result2 = 0;
+
+  // for (int i = 0; i < arr.length; i++) {
+  //   result1 = result1 + arr[i];
+  // }
+  // result2 = result1 / arr.length;
+
+  // print(result2);
+
+  // Задание 5
+  var a = [
+    'a',
+    'b',
+    'c',
+  ];
+  var b = [1, 2, 3];
+  a.add(b.toString());
+  print(a);
+  //Задание 6
+// var lang = ''
 }
